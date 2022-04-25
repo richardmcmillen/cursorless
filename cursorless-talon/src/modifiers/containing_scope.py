8a42e8e945fed5bbf1c154c160300cf0f8d6f1a4
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-04-25 17:15:32 +0100

    Expose SCOPE_LIST_NAMES

--- cursorless-talon/src/modifiers/containing_scope.py
+++ cursorless-talon/src/modifiers/containing_scope.py
@@ -89,6 +89,8 @@ default_values = {
     "surrounding_pair_scope_type": surrounding_pair_scope_types,
 }
 
+SCOPE_LIST_NAMES = list(default_values.keys())
+
 
 def on_ready():
     init_csv_and_watch_changes("modifier_scope_types", default_values)
